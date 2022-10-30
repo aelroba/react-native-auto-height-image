3a4e4443a16f9137e477051127f6ba562210eb1e
Ahmed ElRoba <aalroba@quarafinance.com>
2022-10-30 15:24:07 +0300

    updated data

--- AutoHeightImage.js
+++ AutoHeightImage.js
@@ -1,6 +1,6 @@
 /**
  * @since 2017-04-11 19:10:08
- * @author vivaxy
+ * @author Ahmed ElRoba
  */
 import React, { useEffect, useState, useRef } from 'react';
 import ImagePolyfill from './ImagePolyfill';
